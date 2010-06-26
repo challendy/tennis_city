12cc5365f35085e44434e1d33e978b95991697e4
Chris Hallendy <chris.hallendy@gmail.com>
2010-06-26 12:08:33 -0500

    built nokogiri gem

--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   
   config.gem "rubyist-aasm", :source => "http://gems.github.com", :lib => false
   config.gem "haml", :version => ">3.0.4", :lib => false
-  config.gem "nokogiri", :version => ">1.4.1", :lib => false
+  config.gem "nokogiri", :version => ">=1.4.0", :lib => false
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
