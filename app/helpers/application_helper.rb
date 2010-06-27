f1e5a582ec5ffe3b8d456bef89dda89e001fc5ff
Stephen Korecky <skorecky@gmail.com>
2010-06-27 16:30:59 -0500

    adding new nav styles, added active class to tabs, renamed title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
+  def active_class(path)
+    URI.parse(request.path_info).path =~ /#{path}/ ? 'active' : nil
+  end  
 end
